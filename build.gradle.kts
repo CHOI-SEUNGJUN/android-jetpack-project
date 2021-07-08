ab6c9c8938fbc256455f47f9409d4604d92f0e5d
june <brainyjun@gmail.com>
2021-07-09 00:36:11 +0900

    [UPDATE] Convert groovy gradle script to kotlin dsl script

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Versions.gradle}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // Warning: this repository is going to shut down soon
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
+include(":app")
 rootProject.name = "android-learning-project"
-include ':app'
